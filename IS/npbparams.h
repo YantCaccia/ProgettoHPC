70e9c8cf565a2e90d3f39a1c5038fa2182a3745c
larsyx <gabriele.2000.02@gmail.com>
2024-05-09 23:07:15 +0200

    First Commit

---
+++ IS/npbparams.h
@@ -0,0 +1,14 @@
+#define CLASS 'C'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "08 May 2024"
+#define NPBVERSION "3.4.2"
+#define CC "icpx"
+#define CFLAGS "-O3 -qopenmp -fsycl"
+#define CLINK "$(CC)"
+#define CLINKFLAGS "$(CFLAGS)"
+#define C_LIB "-lm"
+#define C_INC "(none)"
